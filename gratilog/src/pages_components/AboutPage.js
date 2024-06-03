8b45b986941c584d20987de7eae9915ce3e3dfba
Nilushanth Thiruchelvam <nilu.thiru@gmail.com>
2024-06-03 02:02:41 -0400

    initial commit

---
+++ gratilog/src/pages_components/AboutPage.js
@@ -0,0 +1,14 @@
+
+const about = () =>{
+    return (
+        <div id="about">
+            <h1>What is GratiLog ?</h1>
+            <p>
+                GratiLog is a web application designed to help users cultivate a habit of gratitude by logging daily entries of things they are grateful for. The app features a sleek and user-friendly interface, secure authentication, and automated email reminders to encourage consistent use.
+            </p>
+
+            
+
+        </div>
+    );
+}
